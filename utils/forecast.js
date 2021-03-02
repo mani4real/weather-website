c75de2e1593d34a4d6cbe16640a28b2f162270ba
unknown <mani.ebra@gmail.com>
2021-03-02 23:51:49 +0330

    updated weather info

--- utils/forecast.js
+++ utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (longitude, latitude, callback) => {
             callback("Unable to find location")
         } else {
             callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degrees out. but it feels like " +
-                body.current.feelslike + " degrees out.")
+                body.current.feelslike + " degrees out. " + "The humidity is currently " + body.current.humidity + ".")
         }
     })
 }
